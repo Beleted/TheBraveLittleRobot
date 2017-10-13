5c9e8944084eb0959fc73c795a7ec7458f613650
Dan Black <dablack@ea.com>
2017-10-12 21:19:35 -0700

    first movement

---
+++ robot/player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+var direction = Vector2()
+
+const TOP = Vector2(0, -1)
+const DOWN = Vector2(0, 1)
+const LEFT = Vector2(-1, 0)
+const RIGHT = Vector2(1, 0) 
+
+var speed = 0
+const MAX_SPEED = 200
+
+func _ready():
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+   var player_input = Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")
+   
+   direction = Vector2()
+
+   var myglobal = get_node("/root/myglobal")
+
+   if player_input:
+       myglobal.is_moving = true
+       if speed < MAX_SPEED:
+          speed += 50
+       if Input.is_action_pressed("ui_up"):
+          direction += TOP
+       elif Input.is_action_pressed("ui_down"):
+          direction += DOWN
+       elif Input.is_action_pressed("ui_right"):
+          myglobal.is_flipped = false
+          direction += RIGHT
+       elif Input.is_action_pressed("ui_left"):
+          direction += LEFT
+          myglobal.is_flipped = true
+   else:
+      myglobal.is_moving = false
+      speed = 0
+
+   var velocity = direction.normalized() * speed * delta
+
+   print (velocity)
+   move(velocity)
---
+++ robot/run.gd
@@ -0,0 +1,27 @@
+extends AnimatedSprite
+
+var tempElapsed = 0
+
+func _ready():
+   set_process(true)
+   
+func _process(delta):
+   tempElapsed = tempElapsed + delta
+   var myglobal = get_node("/root/myglobal")
+   if myglobal.is_flipped:
+       self.set_flip_h(true)
+   else:
+       self.set_flip_h(false)
+	
+	
+   if myglobal.is_moving:
+       show()
+       if(tempElapsed > 0.1):
+         if(get_frame() == self.get_sprite_frames().get_frame_count("run")-1):
+            set_frame(0)
+         else:
+            self.set_frame(get_frame() + 1)
+      
+       tempElapsed = 0
+   else:
+	   hide()
