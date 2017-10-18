2db015fdb6c299a1eb04f061ff90d4ab3821b557
Mike Bauer <mibauer@ea.com>
2017-10-17 22:00:07 -0700

    getting the timer working ish

--- scripts/splash.gd
+++ scripts/splash.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
 # class member variables go here, for example:
-# var a = 2
+var count = 0
 # var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
--- scripts/splash_timer.gd
+++ scripts/splash_timer.gd
@@ -8,3 +8,8 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+
+func _on_Timer_timeout():
+	self.get_parent().hide()
+	pass # replace with function body
